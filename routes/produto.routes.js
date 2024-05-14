1758cf71d6f038ba975dd9752a60d4d29c676510
devsamuelsilva <devsamuelsilva@gmail.com>
2024-05-13 21:34:51 -0300

    Criando endpoint por id

--- routes/produto.routes.js
+++ routes/produto.routes.js
@@ -5,6 +5,9 @@ export const produtoRoute = (app) => {
     var route = express.Router();
 
     route.get('/', produtoService.getAll);
+    route.get('/:id', produtoService.getById);
+
 
     app.use('/api/produto', route);
+
 }
--- services/produtos.services.js
+++ services/produtos.services.js
@@ -1,9 +1,23 @@
 import { Produtos } from "../model/produto.model.js"
 
 export const produtoService = {
-    getAll: async (req, res) => {
-        const produtos = await Produtos.findAll();
 
-        return res.status(200).json(produtos);
-    }
+        getAll: async (req,res) => {
+            const produtos = await Produtos.findAll();
+            
+            return res.status(200).json(produtos);
+        },
+
+        getById: async (req, res) => {
+            const id = req.params.id;
+            const produto = await Produtos.findByPk(id);
+            
+            if(!produto){
+                return res.status(404).json({
+                    message: `Produto id: ${id} não encontrado`
+                })
+            }
+            return res.status(200).json(produtos);
+        } 
+        
 }
