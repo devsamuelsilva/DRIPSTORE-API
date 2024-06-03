fd698af4dd24077cd04c85dffedf95edf8e64eaf
devsamuelsilva <devsamuelsilva@gmail.com>
2024-06-03 19:42:13 -0300

    Adicionando o CORS a aplicacao

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { connect } from './DB/db.js'
 import { produtoRoute } from './routes/produto.routes.js'
+import { cors } from 'cors'
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
