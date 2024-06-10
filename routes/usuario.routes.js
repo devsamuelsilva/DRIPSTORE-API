5229c53324e312c0d938170ff2a279346579020f
devsamuelsilva <devsamuelsilva@gmail.com>
2024-06-10 20:05:46 -0300

    Implementando o CRUD para a tabela de usuarios

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -6,6 +6,15 @@ export const usuarioRoute = (app) => {
 
     route.get('/', usuarioService.getAll);
 
+    //Rota para criar um usuario implementando as informacoes no postman
+    route.post('/', usuarioService.createUsuario);
+
+    //Rota para atualizar o produto pelo ID
+    route.put('/atualizar/:id', usuarioService.updateusuarioId);
+
+    //Rota para deletar o Usuario
+    route.delete('/delete/:id', usuarioService.deleteUsuarioId);
+
     app.use('/api/usuario', route);
 
 }
--- services/usuario.services.js
+++ services/usuario.services.js
@@ -9,5 +9,74 @@ export const usuarioService = {
         return res.status(200).json(usuarios);
     },
 
+    createUsuario: async (req, res) => {
+            
+        try {
+            const usuario = req.body;
+            //const { nome, descricao, desconto, preco, ativo, categoria, id } = req.body;
+            //const { id, ...produto} = req.body;
+
+            // Criação do produto no banco de dados
+            // const novoProduto = await Produtos.create({nome, descricao, desconto, preco, ativo, categoria, id });
+            const novoUsuario = await Usuarios.create(usuario);
+            
+            return res.status(201).json(novoUsuario);
+
+        } catch (error) {
+
+            res.status(500).json({ error: `Erro ao criar o Usuario: ${error}` });
+        }
+    },
+
+    updateusuarioId: async (req, res) => {
+        const id = req.params.id;
+      
+        // Obtem os dados do produto atualizado do corpo da requisição
+        const dadosUsuarioAtualizado = req.body;
+      
+        try {
+          // Atualiza o produto usando o Sequelize
+          const usuariosAtualizados = await Usuarios.update(
+            dadosUsuarioAtualizado,  // Objeto com as alterações
+            { where: { id } }      // Filtro para atualizar o produto com o ID especificado
+          );
+      
+          if (!usuariosAtualizados) {
+            // Atualização bem-sucedida
+            return res.status(200).json({
+                message: `Usuario com ID ${id} nao atualizado.`
+            });  // Retorna o produto atualizado
+          } 
+          
+            return res.status(200).json({
+                message: `Usuario com ID ${id} atualizado.`
+            });
+
+        } catch (error) {
+          console.error('Erro ao atualizar usuario:', error);
+          return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+        }
+      },
+
+      deleteUsuarioId: async (req, res) => {
+        const id = req.params.id;
+
+        const dadosUsuarioAtualizado = req.body;
+
+        try {
+            const usuario = await Usuarios.findByPk(id);
+        
+            if (!usuario) {
+              return res.status(404).json({ message: 'Usuario não encontrado' });
+            }
+        
+            await usuario.destroy();
+        
+            return res.status(200).json({ message: 'Usuario deletado com sucesso' });
+          } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Erro ao deletar Usuario' });
+          }
+    },
 
 }
